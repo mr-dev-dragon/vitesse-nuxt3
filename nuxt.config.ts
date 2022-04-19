eff99bcbaf45243daf05a8ab312c451bb1caf145
Ahmed Aoulad Amar <71600486+Ahmed-dev-dragon@users.noreply.github.com>
2022-04-19 06:41:15 +0000

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,45 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+export default defineNuxtConfig({
+  meta: {
+    title: 'Vitesse Nuxt 3',
+    link: [
+      {
+        rel: 'icon', type: 'image/png', href: '/nuxt.png',
+      },
+    ],
+  },
+  modules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+  ],
+  autoImports: {
+    dirs: [
+      'composables',
+      'store',
+    ],
+  },
+  experimental: {
+    reactivityTransform: true,
+    // viteNode: true,
+  },
+  vueuse: {
+    ssrHandlers: true,
+  },
+  unocss: {
+    uno: true,
+    attributify: true,
+    preflight: true,
+    icons: {
+      scale: 1.2,
+    },
+    shortcuts: [
+      ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ],
+  },
+  colorMode: {
+    classSuffix: '',
+  },
+})
